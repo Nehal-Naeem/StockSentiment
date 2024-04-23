9be180cec098fbc09b6a808507485650e76e1c64
Nehal <nehal.naeem2@gmail.com>
2024-04-22 22:25:00 -0700

    initial commit

---
+++ Scripts/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import requests
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
+
+
+
