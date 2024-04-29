23502dabfd389adb104a6c1945731044a39c60fa
Nehal <nehal.naeem2@gmail.com>
2024-04-28 19:07:26 -0700

    finished getSrcs fn, started getArticles fn

--- Scripts/newsArticles.py
+++ Scripts/newsArticles.py
@@ -5,26 +5,52 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = os.environ.get("apikey")
-print(api_key)
+api_key = os.environ.get("key")
+# print(api_key)
 
+def getSourceIDs(category):
+    sources_url = "https://newsapi.org/v2/top-headlines/sources"
+    sources_params = {
+        "apiKey":api_key,
+        "category":category,    
+        "language": "en"
+    }
+    
+    response = requests.get(sources_url, sources_params)
+    
+    sources = []
+    for s in response.json().get("sources"):
+        sources.append(s.get("id"))
+    
+    return sources
+
+def parseSourcesArray(arr):
+    out = str(arr).replace("'","")
+    out = out[1:len(out)-1]
+    return out
+
+business = getSourceIDs("business")
+technology = getSourceIDs("technology") 
+sources = business + technology
+
+sourceIDs = parseSourcesArray(sources)
 
 ## append articles cieling of (totalResults/100)
-# def getDescriptions()
+## return article title, descriptions, content,url and publishedAt
+def getArticles(company):
+    url = "https://newsapi.org/v2/everything"
+    params = {
+        "apiKey": api_key,
+        "q": company,
+        "searchin": "title,description",
+        "sources" : sourceIDs,
+        "language":"en",
+        "sort":"publishedAt"
+    }
 
-url = "https://newsapi.org/v2/everything"
+    response = requests.get(url,params)
+    print("totalResults: "+ str(response.json().get("totalResults")))
+    return(response.json().get("articles"))
 
-params = {
-    "apiKey":api_key,
-    "q":"micron",
-    "SearchIn":"title",
-    "page": "1"
-}
 
-response = requests.get(url, params)
-articles = response.json().get("articles")
-counter = 1
-for a in articles:
-    print("article number: "+ str(counter))
-    print(a.get("title"))
-    counter += 1
+getArticles("micron")
